1bf692c3608d2076611676a57c332f2698df1694
Alvin Yip <alvin.y97@gmail.com>
2024-08-27 22:31:18 +1000

    added PUT route

--- backend/index.js
+++ backend/index.js
@@ -48,16 +48,66 @@ app.post('/books', async (request, response) => {
     }
 })
 
+
+//Route to return all books and count
+
 app.get('/books', async (request, response) => {
     try {
         const books = await Book.find({})
 
-        return response.status(200).json(books);
+        return response.status(200).json({
+            count: books.length,
+            data: books
+        });
     } catch (error) {
         console.log(error.message);
         response.status(500).send({ message: error.message })
     }
 })
+
+
+// Route for get individual book via ID
+
+app.get('/books/:id', async (request, response) => {
+    try {
+        const { id } = request.params
+
+        const book = await Book.findById(id)
+
+        return response.status(200).json({
+            requestedBook: book
+        });
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message })
+    }
+})
+
+//Route for updating book
+app.put('/books/:id', async (request, response) => {
+    try {
+        if (
+            !request.body.title ||
+            !request.body.author ||
+            !request.body.publishYear
+        ) {
+            return response.status(400).send({
+                message: 'Please send all required fields: title, author, publishYear',
+            });
+        }
+        const { id } = request.params;
+
+        const result = await Book.findByIdAndUpdate(id, request.body)
+
+        if(!result) {
+            return response.status(404).json({ message: 'Book not found'})
+        }
+
+        return response.status(200).send({ message: 'Book updated successfully'})
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message })
+    }})
 
 
 mongoose
