55cb901419303fe77d0fc0957d543c192e3a07c0
Alvin Yip <alvin.y97@gmail.com>
2024-09-02 21:40:27 +1000

    set up axios

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import Spinner from '../../components/Spinner'
+import { Link } from 'react-router-dom'
+
+
+const Home = () => {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
