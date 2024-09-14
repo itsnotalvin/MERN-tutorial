50b6369b8ac29e5efc7d13d05dbcc2d300724fcf
Alvin Yip <alvin.y97@gmail.com>
2024-09-14 22:59:49 +1000

    delete function

--- frontend/src/pages/DeleteBook.jsx
+++ frontend/src/pages/DeleteBook.jsx
@@ -3,6 +3,7 @@ import BackButton from '../../components/BackButton'
 import Spinner from '../../components/Spinner'
 import axios from 'axios'
 import { useNavigate, useParams } from 'react-router-dom'
+import { Book } from '../../../backend/models/bookModel';
 
 const DeleteBook = () => {
   const [loading, setLoading] =useState(false)
@@ -28,7 +29,18 @@ const DeleteBook = () => {
   return (
     <div className='p-4'>
       <BackButton />
-      <h1 className='text-3xl my-4'>Delete Book</h1>
+      <h1 className='text-3xl my-4'>Delete Book {id}</h1>
+      {loading ? <Spinner /> : ''}
+      <div className='flex flex-col items-center border-2 border-sky-400 rounded-xl w-[600px] p-8 mx-auto'> 
+        <h3 className='text-2xl'>Are you sure you want to delete this book?</h3>
+        <button
+          className='p-4 bg-red-600 text-white m-8 w-full'
+          onClick={handleDeleteBook}
+        >
+          Yes, delete book id: {id}
+
+        </button>
+      </div>
     
     </div>
   )
